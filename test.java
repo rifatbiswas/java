b9ae10cc9576ad21df9e5a698769338ac9bc84e5
rifatbiswas2@gmail.com <rifatbiswas2@gmail.com>
2022-10-11 21:18:38 +0600

    33 added

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ class test {
   public static void main(String[] args) {
    System.out.print("My name is RIFAT 0"); 
    System.out.print("My name is RIFAT 1"); 
-   System.out.print("My name is RIFAT 2"); 
+   System.out.print("My name is RIFAT 2");
+   System.out.print("My name is RIFAT 33"); 
   }
 }

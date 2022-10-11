4a1e64dfec93b621371fb91e550d7c905ff11509
rifatbiswas <rifatbiswas2@gmail.com>
2022-10-11 08:12:49 -0700

    new name added

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 class test {
   public static void main(String[] args) {
-   System.out.print("My name is RIFAT"); 
+   System.out.print("My name is RIFAT 0"); 
+   System.out.print("My name is RIFAT 1"); 
+   System.out.print("My name is RIFAT 2"); 
   }
 }
